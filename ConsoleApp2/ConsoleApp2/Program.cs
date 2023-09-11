806ef04623988e4c98dbce937336779d548fe0d6
gulsimag <85019072+gulsimag@users.noreply.github.com>
2023-09-11 20:56:46 +0300

    SAYI YÜZDE ORANI HESAPLAMA

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sayi, yuzdeoran,sonuc;
+            Console.WriteLine("sayiyi giriniz:");
+            sayi= Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("yuzde kaci alinsin");
+            yuzdeoran = Convert.ToSingle(Console.ReadLine());
+            sonuc = sayi * yuzdeoran / 100;
+
+            Console.WriteLine(sayi+"sayisinin%"+yuzdeoran+"si"+sonuc);
+
+
+
+
+
+            
+            
+        }
+    }
+}
